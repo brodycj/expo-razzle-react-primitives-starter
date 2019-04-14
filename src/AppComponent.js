506393dfa093a838523b2b71eda5d1d5086dce51
Christopher J. Brody <chris@brody.consulting>
2019-04-14 15:05:23 -0400

    Quick test github:brodybits/react-primitives-input

--- src/AppComponent.js
+++ src/AppComponent.js
@@ -5,11 +5,33 @@ import { createElement as h, Component } from 'react';
 
 import { StyleSheet, Text, View } from 'react-primitives';
 
-export default class App extends Component {
+import Input from 'react-primitives-input';
+
+const Input2 = require('react-primitives-input')
+
+class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Input primitive component test</Text>
+        <Input
+          type='text'
+          style={{height: 48}}
+          placeholder='Enter text here'
+        />
+        <Input2
+          style={{height: 48}}
+          placeholder='Enter text here'
+        />
+        <Input
+          style={{height: 48}}
+          placeholder='Enter text here'
+        />
+        <Input
+          type='bogus'
+          style={{height: 48}}
+          placeholder='Enter text here'
+        />
       </View>
     );
   }
@@ -23,3 +45,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
